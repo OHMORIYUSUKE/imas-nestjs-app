0bc4a14f7eea4a71e4b235e64dff5b4800e5de54
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-06-05 17:47:30 +0900

    feat: アイドルの情報を検索

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IGetIdolInfoArray } from 'princess-api-sdk/lib/schemas/Idols/IGetIdolInfo';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('idols')
+  async getIdols(
+    @Query('name') name: string,
+    @Query('age') age: number,
+  ): Promise<IGetIdolInfoArray> {
+    return this.appService.getIdols(name, age);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrincessApiSdk } from 'princess-api-sdk';
+import { IGetIdolInfoArray } from 'princess-api-sdk/lib/schemas/Idols/IGetIdolInfo';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getIdols(name: string, age: number): Promise<IGetIdolInfoArray> {
+    const princessApiSdk = new PrincessApiSdk();
+    // id指定なし
+    const response = await princessApiSdk.getIdolInfo();
+    const result = response.filter((idol) => {
+      if (idol.age === age) return true;
+      if (idol.fullName.indexOf(name) !== -1) return true;
+      if (idol.fullNameRuby.indexOf(name) !== -1) return true;
+      if (idol.displayName.indexOf(name) !== -1) return true;
+      return false;
+    });
+    return result;
   }
 }
