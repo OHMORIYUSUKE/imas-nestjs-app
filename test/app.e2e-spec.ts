5c86749052aec84148382dff63c15936ce926546
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-06-07 16:34:45 +0900

    feat: test e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,109 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/auth/login (POST)', () => {
+    const data = {
+      username: 'john',
+      password: 'changeme',
+    };
+
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(data)
+      .set('Content-Type', 'application/json')
+      .expect(201)
+      .then((response) => {
+        // レスポンスのアクセストークンを取得
+        const { access_token } = response.body;
+
+        // アクセストークンが取得されていることを確認
+        expect(access_token).toBeDefined();
+      });
+  });
+
+  it('/idols?name=春香 (GET)', () => {
+    const data = {
+      username: 'john',
+      password: 'changeme',
+    };
+
+    // ログインリクエストを送信してアクセストークンを取得
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(data)
+      .set('Content-Type', 'application/json')
+      .expect(201)
+      .then((loginResponse) => {
+        const { access_token } = loginResponse.body;
+
+        // アクセストークンを使用して/idols?name=春香にGETリクエストを送信
+        return request(app.getHttpServer())
+          .get('/idols')
+          .query({ name: '春香' })
+          .set('Authorization', `Bearer ${access_token}`)
+          .expect(200)
+          .then((idolsResponse) => {
+            // 適切なレスポンスを受け取ったことを確認
+            expect(idolsResponse.body[0]['fullName']).toEqual('天海 春香');
+            expect(idolsResponse.body.length).toEqual(1);
+          });
+      });
+  });
+
+  it('/idols?name=あまみ (GET)', () => {
+    const data = {
+      username: 'john',
+      password: 'changeme',
+    };
+
+    // ログインリクエストを送信してアクセストークンを取得
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(data)
+      .set('Content-Type', 'application/json')
+      .expect(201)
+      .then((loginResponse) => {
+        const { access_token } = loginResponse.body;
+
+        // アクセストークンを使用して/idols?name=春香にGETリクエストを送信
+        return request(app.getHttpServer())
+          .get('/idols')
+          .query({ name: 'あまみ' })
+          .set('Authorization', `Bearer ${access_token}`)
+          .expect(200)
+          .then((idolsResponse) => {
+            // 適切なレスポンスを受け取ったことを確認
+            expect(idolsResponse.body[0]['fullName']).toEqual('天海 春香');
+            expect(idolsResponse.body.length).toEqual(1);
+          });
+      });
+  });
+
+  it('/idols (GET)', () => {
+    const data = {
+      username: 'john',
+      password: 'changeme',
+    };
+
+    // ログインリクエストを送信してアクセストークンを取得
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(data)
+      .set('Content-Type', 'application/json')
+      .expect(201)
+      .then((loginResponse) => {
+        const { access_token } = loginResponse.body;
+
+        // アクセストークンを使用して/idols?name=春香にGETリクエストを送信
+        return request(app.getHttpServer())
+          .get('/idols')
+          .set('Authorization', `Bearer ${access_token}`)
+          .expect(200)
+          .then((idolsResponse) => {
+            // 適切なレスポンスを受け取ったことを確認
+            expect(idolsResponse.body.length).toEqual(56);
+          });
+      });
+  });
 });
