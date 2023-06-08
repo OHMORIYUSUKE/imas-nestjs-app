3f55c8bc019d688d7546e5ca01ed9b69f27445b5
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-06-08 17:26:08 +0900

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdolsModule } from './modules/idols/idols.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Idole } from './modules/idols/idols.entity';
+import { join } from 'path';
 
 /**
  * ルートモジュール
@@ -17,6 +20,24 @@ import { UsersModule } from './modules/users/users.module';
     UsersModule,
     ConfigModule.forRoot({
       envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        // TypeOrmModuleの設定オブジェクトを作成する
+        const ormConfig: TypeOrmModuleOptions = {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('MYSQL_USER'),
+          password: configService.get('MYSQL_PASSWORD'),
+          database: configService.get('MYSQL_DATABASE'),
+          entities: [join(__dirname + '/**/*.entity{.ts,.js}')],
+          synchronize: true,
+        };
+        return ormConfig;
+      },
+      inject: [ConfigService],
     }),
   ],
   // コントローラを登録する
